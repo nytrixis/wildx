607803f12518fb098d666bef40838713811b9f79
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2024-08-24 18:52:34 +0530

    Carousel updates

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,6 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './Carousel.css';
+import image1 from '../assets/image1.jpg';
 
 const Carousel = () => {
   const settings = {
@@ -16,9 +17,9 @@ const Carousel = () => {
   };
 
   const slides = [
-    { image: '/image1.jpg', text: 'First Slide Text' },
-    { image: '/image2.jpg', text: 'Second Slide Text' },
-    { image: '/image3.jpg', text: 'Third Slide Text' },
+    { image: image1, text: 'First Slide Text', desc: 'A', desc1: 'some text' },
+    { image: '/image2.jpg', text: 'Second Slide Text', desc: 'B', desc1: 'some text' },
+    { image: '/image3.jpg', text: 'Third Slide Text', desc: 'C', desc1: 'some text' },
   ];
 
   return (
@@ -28,6 +29,12 @@ const Carousel = () => {
           <div key={index} className="slide">
             <img src={slide.image} alt={`Slide ${index + 1}`} />
             <div className="slide-number">{`${index + 1}/3`}</div>
+            <div className="slide-desc">{slide.desc}</div>
+            <div className="slide-desc1">{slide.desc1}</div>
+            <div className="slide-buttons">
+              <button className="slide-button">Purchase Tickets</button>
+              <button className="slide-button" id='btn2'>Donate</button>
+            </div>
             <div className="slide-text">{slide.text}</div>
           </div>
         ))}
